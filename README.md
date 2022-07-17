2fe980c78f4c5d7d37fc94c7d7eea06b52df90af
DESKTOP-G7Q67GV\julep <juliancalienni@gmail.com>
2022-07-17 01:31:03 -0300

    Templates resumen, actualizacion personas y app

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # estaqueros
-Aplicacion para definir cuentas en las juntadas
-prerequisitos:
-python 3.8.7
+Aplicacion para definir cuentas en las juntadas  
+prerequisitos:  
+python 3.8.7  
 
-instalar las siguientes librerias de python:
--pip install flask
+instalar las siguientes librerias de python:  
+-pip install flask  
--- estaqueros/app.py
+++ estaqueros/app.py
@@ -15,5 +15,15 @@ def calcular():
     test = int(request.form['cantidad'])
     return render_template('personas.html', cantidad = test, indice = 0)
 
+@app.route("/resumen", methods=['POST'])
+def resumen():
+    personas = []
+    montos = []
+    cantidad = int(len(request.form)/2)
+    for persona in range(1, (cantidad+1)):
+        personas.append(request.form["p"+str(persona)])
+        montos.append(request.form["c"+str(persona)])
+    return render_template('resumen.html', personas = personas, montos = montos)
+
 if __name__ == "__main__":
     app.run()
